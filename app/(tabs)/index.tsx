08cc9c2e47a08d559a61eac271c405c9a5813d96
Justin Yang <justiny_2013@yahoo.com>
2024-11-10 20:00:29 -0800

    added dropdown accordion of workouts for each day

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,37 +5,100 @@ import {
   Platform,
   View,
   Pressable,
+  TouchableOpacity,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
 import { FlatList } from "react-native-gesture-handler";
+import { useEffect, useState } from "react";
+import { useSQLiteContext } from "expo-sqlite";
+import Animated, { SharedValue, useAnimatedStyle, useDerivedValue, useSharedValue, withTiming } from "react-native-reanimated";
 
-const daysOfWeek = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
+function AccordionItem({
+  isExpanded,
+  children,
+  viewKey,
+  duration = 500,
+}: { isExpanded: SharedValue<boolean>, children: any, viewKey: string, duration?: number }) {
+  const height = useSharedValue(0);
 
-export default function Index() {
-  const renderItem = ({ item }: { item: string }) => {
-    return (
-      <Link
+  const derivedHeight = useDerivedValue(() =>
+    withTiming(height.value * Number(isExpanded.value), {
+      duration,
+    })
+  );
+  const bodyStyle = useAnimatedStyle(() => ({
+    height: derivedHeight.value,
+  }));
+
+  return (
+    <Animated.View
+      key={`accordionItem_${viewKey}`}
+      style={[accordionStyles.animatedView, bodyStyle]}>
+      <View
+        onLayout={(e) => {
+          height.value = e.nativeEvent.layout.height;
+        }}
+        style={accordionStyles.wrapper}>
+        {children}
+      </View>
+    </Animated.View>
+  );
+}
+
+const DayItem = ({ item }: { item: Day }) => {
+  const open = useSharedValue(false);
+  const onPress = () => {
+    open.value = !open.value;
+  };
+
+  const temp: string[] = [
+    'Workout 1',
+    'A very long workout name that no one knows about',
+  ];
+
+  return (
+    <View style={itemStyles.mainContainer}>
+      {/* <Link
         href={{
           pathname: "/routine",
-          params: { dayOfWeek: item },
+          params: { dayOfWeek: item.day },
         }}
         asChild
-      >
-        <Pressable style={itemStyles.container}>
-          <Text style={itemStyles.text}>{item}</Text>
-        </Pressable>
-      </Link>
-    );
-  };
+      > */}
+      <Pressable style={itemStyles.dayContainer} onPress={onPress}>
+        <Text style={itemStyles.dayText}>{item.day}</Text>
+      </Pressable>
+      {/* </Link> */}
+      <AccordionItem isExpanded={open} viewKey="Accordion">
+        {temp.map((workout: string, idx: number) =>
+          <TouchableOpacity style={itemStyles.workoutContainer} key={`${item.id}-${idx}`}>
+            <Text style={itemStyles.workoutText}>{workout}</Text>
+          </TouchableOpacity>
+        )}
+      </AccordionItem>
+    </View>
+  );
+};
+
+const renderItem = ({ item }: { item: Day }) => <DayItem item={item} />;
+
+interface Day {
+  id: number;
+  day: string;
+};
+
+export default function Index() {
+  const db = useSQLiteContext();
+  const [days, setDays] = useState<Day[]>([]);
+
+  useEffect(() => {
+    const setup = async () => {
+      const allDays = await db.getAllAsync<Day>('SELECT * FROM days');
+      setDays(allDays);
+    }
+    setup();
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -45,9 +108,11 @@ export default function Index() {
       </View>
       <View style={styles.mainContainer}>
         <FlatList
-          data={daysOfWeek}
+          data={days}
           renderItem={renderItem}
+          keyExtractor={(item: Day) => `${item.id}`}
           contentContainerStyle={{ gap: 16 }}
+          showsVerticalScrollIndicator={false}
         />
       </View>
       <StatusBar style={Platform.OS === "ios" ? "dark" : "auto"} />
@@ -62,11 +127,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     marginHorizontal: 30,
+    marginBottom: 30,
   },
   headerContainer: {
     alignItems: "center",
     gap: 10,
     marginVertical: 20,
+    marginBottom: 20,
   },
   header: {
     fontWeight: "bold",
@@ -79,14 +146,44 @@ const styles = StyleSheet.create({
 });
 
 const itemStyles = StyleSheet.create({
-  container: {
+  mainContainer: {
+    borderRadius: 10,
+    backgroundColor: "#fff",
+  },
+  dayContainer: {
     backgroundColor: "#fff",
     alignItems: "center",
-    padding: 20,
+    paddingVertical: 16,
     borderRadius: 10,
   },
-  text: {
+  workoutContainer: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    padding: 12,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    borderTopWidth: 1,
+    borderColor: 'lightgrey',
+    width: '100%',
+  },
+  dayText: {
     fontSize: 20,
     fontWeight: "bold",
+  },
+  workoutText: {
+    fontSize: 16,
+  }
+});
+
+const accordionStyles = StyleSheet.create({
+  animatedView: {
+    width: '100%',
+    overflow: 'hidden',
+  },
+  wrapper: {
+    width: '100%',
+    position: 'absolute',
+    display: 'flex',
+    alignItems: 'center',
   },
 });
